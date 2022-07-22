4159294eb2a76c4602116b14ee81dc446418a1ad
Luis Eduardo <luis.ratis04@gmail.com>
2022-07-21 19:46:49 -0500

    Cria a rota de listar produtos

--- src/main/java/com/example/demo/controladores/ProdutoControlador.java
+++ src/main/java/com/example/demo/controladores/ProdutoControlador.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,9 +18,12 @@ public class ProdutoControlador {
     @PostMapping(value = "/produto",consumes = MediaType.APPLICATION_JSON_VALUE)
 
     public ResponseEntity<?> salvar(@RequestBody Produto produto){
-        System.out.println(produto.getValor());
         produtoRepositorio.save(produto);
         return new ResponseEntity<>(produto, HttpStatus.CREATED);
     }
-
+    @GetMapping(value = "/produto")
+    public ResponseEntity<?> listar(){
+        Iterable<Produto> produtos = produtoRepositorio.findAll();
+        return new ResponseEntity<>(produtos, HttpStatus.OK);
+    }
 }
